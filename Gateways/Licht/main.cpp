192badaa5fd6d989b08a163953d175d57fc70d50
Tobias <github@do1tsp.de>
2020-11-10 21:39:56 +0100

    Create main.cpp

---
+++ Gateways/Licht/main.cpp
@@ -0,0 +1,25 @@
+// pixelscolor soll von PI  gesteuert werden, 
+// Daten sollen gespeichert werden im EEPROM sodas eine Datenverbinung nur bei Veränderung
+// aufgebaut werden muss 
+
+#include <Adafruit_NeoPixel.h> //Libary extra einbinden
+#include <avr/power.h> 
+
+#define PIN            6
+#define NUMPIXELS      10
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500;
+
+void setup() {
+  pixels.begin();
+}
+
+void loop() {
+  for(int i=0;i<NUMPIXELS;i++){
+    pixels.setPixelColor(i, pixels.Color(255,255,255)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+}
