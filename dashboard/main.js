e7559ec51b8f50bfaca4bce53266c916cb2b8d2b
Tobias <github@do1tsp.de>
2021-05-02 19:24:09 +0200

    Javascript based Dashboard
    Home ist die default Seite, 
    
    danach kann über die Navigationsleiste auf andere Oberflächen Geschaltet werden
    
    Es sind immernoch Beispiel daten im Quellcode vorhanden der nach und Nach aussortiert wird
    
    auch besteht noch ein Problem beim wechseln auf die Seite Spotify bei Fehlender Wiedergabe und damit Fehlender Playlist

--- dashboard/main.js
+++ dashboard/main.js
@@ -1,25 +1,56 @@
 function httpGet(theUrl) {
-    let xmlHttpReq = new XMLHttpRequest();
-    xmlHttpReq.open("GET", theUrl, false); 
-    xmlHttpReq.send(null);
-    return xmlHttpReq.responseText;
-  } // Function um mit NodeRed zu Sprechen
+  let xmlHttpReq = new XMLHttpRequest();
+  xmlHttpReq.open("GET", theUrl, false); 
+  xmlHttpReq.send(null);
+  return xmlHttpReq.responseText;
+  } 
+function Clear() {
+  document.getElementById("Home").innerHTML = "";
+  document.getElementById("Spotify").innerHTML = "";
+  document.getElementById("DAB").innerHTML = "";
+  document.getElementById("Settings").innerHTML = "";
+}
 
-  window.onload = console.log(httpGet('http://localhost:1880/track')); // Titel - Künstler
-  window.onload = document.getElementById('Cover').innerHTML = '<img src="' + httpGet('http://localhost:1880/cover') + '"style="width: 100px; height: 100px;"/>'; // Setzt das Cover in das DIV Cover
-  
-  // Titellänge
-  const duration = httpGet('http://localhost:1880/duration');
-  const datum_duration = new Date(1970,1,1,0,0,0, duration);      
-  window.onload = console.log(datum_duration.getMinutes());
-  window.onload = console.log(datum_duration.getSeconds());
+function addHome() {
+  Clear();
+  document.getElementById("Home").innerHTML = "Hallo";
+  document.getElementById("Home").classList.add("active");
+ }
+ 
+function addSpotify() {
+  Clear();
+  document.getElementById("Spotifynav").classList.add("active");
+  var table = document.createElement("table");
+  table.setAttribute("class", "table table-dark table-hover Playlist");
+  table.setAttribute("id", "playlisttable");          
+  document.getElementById("Spotify").appendChild(table);     
+  document.getElementById("playlisttable").innerHTML = '<thead></thead> <tbody></tbody>';
+  document.getElementById("playlisttable").firstChild.innerHTML = '<tr><th scope="col">#</th><th scope="col">Titel</th><th scope="col">Interpret</th></tr>';
+  var playlisttitel = "";
+  const json = httpGet('http://localhost:1880/getPlaylist');
+  const obj = JSON.parse(json);
+  var length = (obj.length-1);
+      for (var i = 0; i <= length ; i++) {
+      playlisttitel = playlisttitel + '<tr onclick="' + "getSong('" + obj[i].track.id + "') " + '"><th> ' + (i+1) + '</th> <td>' +  obj[i].track.name + '</td> <td>' + obj[i].track.artists[0].name + '</td></tr> ';
+    }
+  document.getElementById("playlisttable").lastChild.innerHTML = playlisttitel;
+  document.getElementById('cover').innerHTML = '<img src="' + httpGet('http://localhost:1880/cover') + '" style="width: 100px; height: 100px;"/>'; 
+  document.getElementById('media').innerHTML = httpGet('http://localhost:1880/track');
+}
 
-  // Aktuelle Titelzeit
-  const progress = httpGet('http://localhost:1880/progress');
-  const datum_progress = new Date(1970,1,1,0,0,0, progress);      
-  window.onload = console.log(datum_progress.getMinutes());
-  window.onload = console.log(datum_progress.getSeconds());
+function addDAB(){
+  Clear();
+  document.getElementById("DAB").innerHTML = 'DAB';
+  document.getElementById("dabnav").classList.add("active");
+}
 
-  // Zeitanzeige
-  window.onload = document.getElementById('Uhr-Zeit').innerHTML = '<p>' + '12' + ':' + '00' + '</p>'; // Setzt die Uhrzeit
-  window.onload = document.getElementById('Uhr-Datum').innerHTML = '<p>' + '21.04.2021' + '</p>'; // Setzt das Datum
+function addSettings (){
+  Clear();
+  document.getElementById("Settings").innerHTML = 'Settings';
+  document.getElementById("Settingsnav").classList.add("active");
+
+}
+function getSong(id){
+  console.log(id);
+
+}
